ae8dd1c24948cd0e4595c82c882d980a3c5818fc
avilash b <91630253+AvilashBhowmick12@users.noreply.github.com>
2024-10-27 12:03:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ const closeIcon = document.getElementById('close-icon');
 
 // Store the movie data in an array for easier manipulation
 const movies = [
+    { title: " Do Patti 2024 - netflix ", image: "images/movie166.jpg", link: "    https://drive.google.com/file/d/1Oc_3Q_6Bc-nBANLF9_39XktGRU7NXQtV    " },
     { title: " Transformers 6: Rise of the Beasts 2023 ", image: "images/movie165.jpg", link: "    https://drive.google.com/drive/folders/12lPcNIWCZ0sNBE75xagOCuCLyTRk6IxW    " },
     { title: " Venom 3 The Last Dance 2024 ", image: "images/movie164.jpg", link: "    https://drive.google.com/drive/folders/1Ri3tfLx5Z7-VCCMoxWOQaRjEhIM1a0Cm    "},
     { title: " Venom 2 Let there be carnage 2021 ", image: "images/movie163.jpg", link: "    https://drive.google.com/drive/folders/17mXgYrSWnJvcd8FwRpDNxTFpiWuMuFwB    "},

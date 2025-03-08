3a51c371f09a8986253a3a33eb2b1c2a5b761dda
avilash b <91630253+AvilashBhowmick12@users.noreply.github.com>
2025-03-08 13:24:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ const closeIcon = document.getElementById('close-icon');
 
 // Store the movie data in an array for easier manipulation
 const movies = [
+    { title: " Daredevil : Born Again 2025 ", image: "images/movie199.jpg", link: "    https://drive.google.com/drive/folders/120lJ8LtodwLhircn-F5zx-PFJTJEWWAk    " },
     { title: " Undercover High School 2025 ", image: "images/movie198.jpg", link: "    https://drive.google.com/drive/folders/1vo_RYb46Q8tQKFTNmUbtTG4JvCQmLV8o    " },
     { title: " Chhaava 2025 ", image: "images/movie197.jpg", link: "    https://drive.google.com/drive/folders/1CJbGqwEA9bvxh-YQyX4N1_epiN6ajwVP    " },
     { title: " Wonderland 2024 ", image: "images/movie196.jpg", link: "    https://drive.google.com/drive/folders/1WPm-34VZ2k4OFpcwCyaCkATg6QJrTFOx    " },

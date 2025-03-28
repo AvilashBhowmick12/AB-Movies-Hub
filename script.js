49a401fc8bc9e75001c4cbc9c354ae314e83fd9a
avilash b <91630253+AvilashBhowmick12@users.noreply.github.com>
2025-03-28 23:36:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ const closeIcon = document.getElementById('close-icon');
 
 // Store the movie data in an array for easier manipulation
 const movies = [
+    { title: " Zakir Khan Delulu Express 2025 - AmazonPrime ", image: "images/movie204.jpg", link: "    https://drive.google.com/drive/folders/1DwpgR928qSwEdXISyRhVQzfLjmiGcG_b    " },
     { title: " Khakee The Bengal Chapter ", image: "images/movie203.jpg", link: "    https://drive.google.com/drive/folders/1N18bars9tMPHvWI4CriYqLLpSvWoEhpO    " },
     { title: " The Secret Life of Pets 2 2019 ", image: "images/movie202.jpg", link: "    https://drive.google.com/drive/folders/11XKAuuS3az18JlKkDd22tZjC61o96C6n    " },
     { title: " The Secret Life of Pets 2016 ", image: "images/movie201.jpg", link: "    https://drive.google.com/drive/folders/10I6D4p3GMmntAflt1oFXyfY2vYqjkFi0    " },

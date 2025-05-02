b11a703aa34af2325a185136ba12ba7a8c1a6334
avilash b <91630253+AvilashBhowmick12@users.noreply.github.com>
2025-05-02 23:05:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ const closeIcon = document.getElementById('close-icon');
 
 // Store the movie data in an array for easier manipulation
 const movies = [
+    { title: " Bhog S1 2025 Hoichoi ", image: "images/movie215.jpg", link: "    https://drive.google.com/drive/folders/1m_NxM-Xwlzpzs4Iw7Xr0Tgh5PsElfetA    " },
     { title: " Raid 2 2025 ", image: "images/movie214.jpg", link: "    https://drive.google.com/drive/folders/17bjY0TkD4UfzUoqBF6rhv2ZHr05M17xK    " },
     { title: " Ground Zero (2025) ", image: "images/movie213.jpg", link: "    https://drive.google.com/drive/folders/1wsYZdeVTPNIipAFMy72EGIg5Fb2NQO-A    " },
     { title: " Kesari Chapter 2 (2025) ", image: "images/movie212.jpg", link: "    https://drive.google.com/drive/folders/1Avzw4BQHTZXNEnIVFv-baxXhl6qddDIp    " },

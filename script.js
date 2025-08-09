2eebfdfb56733b125e017bcff5590bfbea908dfe
avilash b <91630253+AvilashBhowmick12@users.noreply.github.com>
2025-08-09 23:32:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,6 +18,8 @@ const closeIcon = document.getElementById('close-icon');
 
 // Store the movie data in an array for easier manipulation
 const movies = [
+    { title: " Materialists (2025) ", image: "images/movie252.jpg", link: "    https://drive.google.com/drive/folders/1joGy4_2LTgTcImA3nTYSNKQ-FZVGVY6h    " },
+    { title: " Salakaar (2025) ", image: "images/movie251.jpg", link: "    https://drive.google.com/drive/folders/1m0_SLXLRx5KLIOAQ02e3TYHTe4OExuP2    " },
     { title: " Wednesday", image: "images/movie250.jpg", link: "    https://drive.google.com/drive/folders/1pXTLcrqGtV8lnIA2IWO7OZjqaW9fb3Vj    " },
     { title: " Connect S01 2022", image: "images/movie249.jpg", link: "    https://drive.google.com/drive/folders/1SKbkJ8wFVtSxLPnqrdtCWmQlAQIHAUX2    " },
     { title: " Jurassic World Rebirth 2025", image: "images/movie248.jpg", link: "    https://drive.google.com/drive/folders/1ecPMrgvjsEgOH2a3FgxwBw8TVIHrzZnD    " },
